d8d4f8db5d7669a932701c63b2b79a4c604a7a67
valero1212 <marcvalero12@hotmail.com>
2017-02-10 19:15:19 +0100

    Add files via upload

---
+++ Proba_git.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ruben
+ */
+public class Proba_git {
+    public static void main(String Args[]){
+        System.out.println("Hola Mon");
+        System.out.println("Adeu Món");
+    }
+}
